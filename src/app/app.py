c26718814dca4ae4c07912ca2abe825c346bbd6e
iontom <iontom@gmail.com>
2020-11-23 23:17:49 -0800

    updating before lift and refactor

--- src/app/app.py
+++ src/app/app.py
@@ -1,9 +1,10 @@
 import os
 import pandas as pd
 from redis import Redis
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_bootstrap import Bootstrap
 # from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
@@ -16,66 +17,21 @@ database_uri = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}:{dbport}/{dbnam
     dbname=os.environ['DBNAME'],
     dbport=os.environ['DBPORT']
 )
-
-# app.config.update(
-#     SQLALCHEMY_DATABASE_URI=database_uri,
-#     SQLALCHEMY_TRACK_MODIFICATIONS=False,
-# )
-
-# # initialize the database connection
-# db = SQLAlchemy(app)
-
-# db_string = "postgres://admin:donotusethispassword@aws-us-east-1-portal.19.dblayer.com:15813/compose"
-
 db = create_engine(database_uri)
 
-# Create 
-# db.execute("CREATE TABLE IF NOT EXISTS films (title text, director text, year text)")  
-# db.execute("INSERT INTO public.films (title, director, year) VALUES ('Doctor Strange', 'Scott Derrickson', '2016')")
-
-
+# tpath = '/code/src/app/templates'
+app = Flask(__name__, template_folder='./templates')
 
 @app.route('/')
-def home():    
-    # Read
-    # result_set = db.execute("SELECT * FROM public.film_test").fetchone()
+@app.route('/index')
+def home():
     df = pd.read_sql_query('SELECT * FROM public.film_test',con=db)
-    # print('result set', result_set)
-    # r = result_set['film_name'][0]
+    # return df.to_html()
+    print('debug')
+    table_out = df.to_html().replace('class="dataframe"', 'class="table table-striped"')
+    print(table_out)
 
-
-    # for r in result_set:  
-    #     print(r)
-    #     return(r)
-    # Update
-    # db.execute("UPDATE public.films SET title='Some2016Film' WHERE year='2016'")
-    # Delete
-    # db.execute("DELETE FROM public.films WHERE year='2016'")  
-    print('hello world')
-    return df.to_html()
-    # from models import Home
-    # home = Home.query.all()
-    # return render_template('home.html')
-    # redis.incr('hits')
-    # return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
-
-# @app.route('/register', methods=['GET'])
-# def view_registration_form():
-#     return render_template('guest_registration.html')
-
-
-# @app.route('/register', methods=['POST'])
-# def register_guest():
-#     from models import Guest
-#     name = request.form.get('name')
-#     email = request.form.get('email')
-
-#     guest = Guest(name, email)
-#     db.session.add(guest)
-#     db.session.commit()
-
-#     return render_template(
-#         'guest_confirmation.html', name=name, email=email)
+    return render_template('home.html', title='Space!', df_table=table_out)
 
 
 if __name__ == "__main__":
